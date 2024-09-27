e9560bd6aef7ef49a9fac555879d5588d5f3e071
Rony Hossain <mdrony.ain@gmail.com>
2024-09-27 18:57:22 +0600

    first commit

--- src/components/body/home/Silder.tsx
+++ src/components/body/home/Silder.tsx
@@ -7,7 +7,8 @@ import {
   } from "@/components/ui/carousel"
 import { getSliders } from "@/utils/apis/slider";
 import Image from 'next/image';
-  
+
+// {process.env.NEXT_PUBLIC_BACKEND_BASE_URL + slider.attributes.image.data.attributes.url } 
 
 const Silder = async () => {
     const sliders = await getSliders();
@@ -19,7 +20,7 @@ const Silder = async () => {
         <CarouselContent className=" h-[400px] bg-red-400 mt-10 relative">
         {
             sliders.map((slider: any) => (
-                <CarouselItem className="mx-auto" key={slider.attributes.name}> <Image alt="" fill src={process.env.NEXT_PUBLIC_BACKEND_BASE_URL + slider.attributes.image.data.attributes.url }  /> </CarouselItem>
+                <CarouselItem className="mx-auto" key={slider.attributes.name}> <Image alt="" fill src="/slider1.png" /> </CarouselItem>
             ))
         }
            
